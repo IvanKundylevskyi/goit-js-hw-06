e549f19ce74726a26487ef2b80f4623422773ce9
Ivan <119350481+IvanKundylevskyi@users.noreply.github.com>
2023-08-30 22:24:48 +0300

    main

--- js/task-01.js
+++ js/task-01.js
@@ -2,10 +2,9 @@ const categoryItems = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categoryItems.length}`);
 
 categoryItems.forEach(category => {
-
   const categoryTitle = category.querySelector('h2').textContent;
-  const categoryItemsCount = category.querySelectorAll('li').length;
-
+  const categoryItemsList = category.querySelectorAll('li');
+  const categoryItemsCount = categoryItemsList.length;
   console.log(`Category: ${categoryTitle}`);
   console.log(`Elements: ${categoryItemsCount}`);
 });
